53a9e2df472cc1920efaf1871914ec7b68e4b3ed
Destiny Serna <destinyeserna@gmail.com>
2025-03-25 23:49:09 -0700

    Added restricton base < 10 and added base prefixes

--- base_converter.py
+++ base_converter.py
@@ -7,7 +7,7 @@ def convert_binary_to_decimal(binary_value):
         value_to_convert = value_to_convert[:-1]   
         decimal_result += int(binary_digit) * (2**current_digit)
         current_digit += 1
-    return decimal_result
+    return str(decimal_result)
 
 def convert_decimal_to_binary(decimal_value):
     value_to_convert = decimal_value
@@ -18,22 +18,31 @@ def convert_decimal_to_binary(decimal_value):
     return binary_result
 
 def convert_base_to_decimal(value, base):
+    if base >10:
+        return 'Base must be less than 10'
     value_to_convert = str(value)
     decimal_result = 0
     current_digit = 0
     while len(value_to_convert) > 0:
-        binary_digit = value_to_convert[-1]
+        next_digit = value_to_convert[-1]
         value_to_convert = value_to_convert[:-1]   
-        decimal_result += int(binary_digit) * (base**current_digit)
+        decimal_result += int(next_digit) * (base**current_digit)
         current_digit += 1
-    return decimal_result
+    return str(decimal_result)
 
 def convert_decimal_to_base(value, base):
-    value_to_convert = value
-    binary_result = ''
+    if base >10:
+        return 'Base must be less than 10'
+    value_to_convert = int(value)
+    converted_digit = ''
     while value_to_convert >= 1:
-        binary_result = str(value_to_convert % base) + binary_result
+        next_digit = str(value_to_convert % base)
+        converted_digit =  next_digit + converted_digit
         value_to_convert = int(value_to_convert / base)
-    return binary_result
+    if base == 2:
+        converted_digit = '0b'+converted_digit
+    elif base == 8:
+        converted_digit = '0o'+converted_digit
+    return converted_digit
 
-print(convert_decimal_to_base(1977,8))
+print(convert_decimal_to_base(1977,12))
