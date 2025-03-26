7ce57806ac51eca822d18823506b4cf6b5c207e9
Destiny Serna <destinyeserna@gmail.com>
2025-03-25 23:28:50 -0700

    Added function to convert binary to decimal

--- base_converter.py
+++ base_converter.py
@@ -7,3 +7,17 @@ def convert_decimal_to_binary(decimal_value):
     return binary_result
 
 print(convert_decimal_to_binary(11))
+
+
+def convert_binary_to_decimal(binary_value):
+    value_to_convert = str(binary_value)
+    decimal_result = 0
+    current_digit = 0
+    while len(value_to_convert) > 0:
+        binary_digit = value_to_convert[-1]
+        value_to_convert = value_to_convert[:-1]   
+        decimal_result += int(binary_digit) * (2**current_digit)
+        current_digit += 1
+    return decimal_result
+
+print(convert_binary_to_decimal(1011))
